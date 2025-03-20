f7701d2da5a052b8616f8398dd7fd3f1a6c591fc
Malupula Ashok <malupulaashok@Malupulas-MacBook-Pro.local>
2025-03-21 02:01:51 +0530

    Prepare for Render deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn track_expense.wsgi:application
--- track_expense/settings.py
+++ track_expense/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5ui44=do6)8rqc2^1g5%n54r3yvq69qm(qz9=pc)ay*g+l2&vh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ExpenseTracker.onrender.com', '*']
 
 
 # Application definition
@@ -116,7 +116,8 @@ LOGIN_URL = '/accounts/login/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
