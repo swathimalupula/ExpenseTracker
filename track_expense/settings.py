ea059be051ca160a4152ee506c4653a285e01bb1
Malupula Ashok <malupulaashok@Malupulas-MacBook-Pro.local>
2025-03-21 02:31:37 +0530

    fixed basic web page

--- track_expense/settings.py
+++ track_expense/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'track_expense.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'tracker', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
